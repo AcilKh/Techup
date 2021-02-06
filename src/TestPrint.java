dede7d4c23735c8d1f07dcda9c183ae2ebeb35cc
AcilKh <acilpro@outlook.fr>
2021-02-06 18:52:00 +0100

    Add second message in branch TestPrint-branch

--- src/TestPrint.java
+++ src/TestPrint.java
@@ -2,6 +2,8 @@
 public class TestPrint {
 	
 	public static void main(String[] args) {
+
 		System.out.println("test nouvelle branche");
+		System.out.println("Second test in order to merge in branch master");
 	}
 }
