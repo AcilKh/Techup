00c7d8ce7fad7dd4a1bb09cb27db8768663f16ab
AcilKh <acilpro@outlook.fr>
2021-02-06 22:10:39 +0100

    Delete the first src repository

--- src/TestPrint.java
+++
@@ -1,9 +0,0 @@
-
-public class TestPrint {
-	
-	public static void main(String[] args) {
-
-		System.out.println("test nouvelle branche");
-		System.out.println("Second test in order to merge in branch master");
-	}
-}
